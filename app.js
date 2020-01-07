fa9a2b53b07b6fb0769d51540d3e3271e3566cb0
PlayboiCarti <46751826+Kais2kz@users.noreply.github.com>
2020-01-07 23:06:11 +0100

    allt

---
+++ app.js
@@ -0,0 +1,9 @@
+var con = mysql.createConnection({
+host: "localhost",
+user: ”root",
+password: ""
+});
+con.connect(function(err) {
+if (err) throw err;
+console.log(”Anslutningen Lyckades!");
+});
